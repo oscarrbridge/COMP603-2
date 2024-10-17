6a966e8af5e3fac9d841cff6f2eb4e664eb9e670
Riley <jbk4828@autuni.ac.nz>
2024-10-18 11:23:29 +1300

    Roulette and Wheel Creation

--- src/dialogRoulette.java
+++ src/dialogRoulette.java
@@ -1,7 +1,19 @@
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class dialogRoulette extends javax.swing.JDialog {
 
-    frmGamesHome parent; // Test
+    frmGamesHome parent;
+    
+    Wheel wheel = new Wheel();
+    ArrayList board = wheel.board();
 
     public dialogRoulette(frmGamesHome parent) {
         super(parent, true);
@@ -10,7 +22,6 @@ public class dialogRoulette extends javax.swing.JDialog {
         this.parent = parent;
     }
 
-    @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -30,7 +41,76 @@ public class dialogRoulette extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    private void btnOptionOneMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnOptionTwoMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnOptionThreeMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnOptionFourMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnOptionFiveMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnOptionSixMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnOptionSevenMousePressed(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+    private void btnSpinWheel(java.awt.event.MouseEvent evt)
+    {
+        
+    }
+    
+   private void btnExitMousePressed(java.awt.event.MouseEvent evt) {                                     
+        this.hide();
+    } 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
+
+   private int spinResult()
+   {
+       return wheel.Spin();
+   }
+   
+   private String wheelColour(int spin) throws IOException
+   {
+       String colour = "";
+
+        FileReader Fr;
+        try {
+            Fr = new FileReader("resources/RouletteColours.txt");
+            BufferedReader Br = new BufferedReader(Fr);
+
+        for(int k = 0; k<spin-1; k++)
+        {
+            colour = Br.readLine();
+        }
+        Br.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(dialogRoulette.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return colour;
+   }
 }
